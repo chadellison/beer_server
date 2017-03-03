0f1780f6ae211e846cb7fa35b6c92aa29c35486a
Chad Ellison <chad.ellison0123@gmail.com>
2017-03-03 12:48:41 -0700

    reconfig email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,26 +74,43 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.smtp_settings = {
-    openssl_verify_mode: OpenSSL::SSL::VERIFY_NONE,
-    ssl: true,
-    enable_starttls_auto: true,
-    address:        "",
-    port:           "",
-    domain:         "gmail.com",
-    authentication: "login",
-    user_name:      ENV["email_user_name"],
-    password:       ENV["email_password"]
-  }
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = false
+  
+  # config.action_mailer.smtp_settings = {
+  #   openssl_verify_mode: OpenSSL::SSL::VERIFY_NONE,
+  #   ssl: true,
+  #   enable_starttls_auto: true,
+  #   address:        "",
+  #   port:           "",
+  #   domain:         "gmail.com",
+  #   authentication: "login",
+  #   user_name:      ENV["email_user_name"],
+  #   password:       ENV["email_password"]
+  # }
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
+
+
+  config.action_mailer.default_url_options = { host: "http://beer-server0123.herokuapp.com" }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.default :charset => "utf-8"
+    config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "http://beer-server0123.herokuapp.com",
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: ENV["email_user_name"],
+      password: ENV["email_password"]
+    }
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
