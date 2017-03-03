de0509fb92fc89dc8781fda03a8fd26360c227bf
Chad Ellison <chad.ellison0123@gmail.com>
2017-03-03 14:47:37 -0700

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,11 +55,9 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "beer_server_#{Rails.env}"
-  config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -75,42 +73,39 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
 
   # config.action_mailer.smtp_settings = {
   #   openssl_verify_mode: OpenSSL::SSL::VERIFY_NONE,
   #   ssl: true,
   #   enable_starttls_auto: true,
   #   address:        "",
-  #   port:           "",
-  #   domain:         "gmail.com",
+  #   port:           587,
+  #   domain:         "http://beer-server0123.herokuapp.com",
   #   authentication: "login",
   #   user_name:      ENV["email_user_name"],
   #   password:       ENV["email_password"]
   # }
+
+  config.action_mailer.smtp_settings = {
+    openssl_verify_mode: "none",
+    site: ENV["host"],
+    enable_starttls_auto: true,
+    address:        "smtp.gmail.com",
+    port:           587,
+    domain:         "gmail.com",
+    authentication: "login",
+    user_name:      ENV["email_user_name"],
+    password:       ENV["email_password"]
+  }
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
-
-
-  config.action_mailer.default_url_options = { host: "http://beer-server0123.herokuapp.com" }
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.perform_deliveries = true
-    config.action_mailer.raise_delivery_errors = false
-    config.action_mailer.default :charset => "utf-8"
-    config.action_mailer.smtp_settings = {
-      address: "smtp.mailgun.org",
-      port: 587,
-      domain: "http://beer-server0123.herokuapp.com",
-      authentication: "plain",
-      enable_starttls_auto: true,
-      user_name: ENV["email_user_name"],
-      password: ENV["email_password"]
-    }
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
