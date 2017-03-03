7430e8371ae55e9f09107900f5c04327f0299835
Chad Ellison <chad.ellison0123@gmail.com>
2017-03-03 14:27:03 -0700

    adjust config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,9 +103,9 @@ Rails.application.configure do
     config.action_mailer.raise_delivery_errors = false
     config.action_mailer.default :charset => "utf-8"
     config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
+      address: "smtp.mailgun.org",
       port: 587,
-      domain: "http://beer-server0123.herokuapp.com",
+      domain: "beerproject0123mailgun.com",
       authentication: "plain",
       enable_starttls_auto: true,
       user_name: ENV["email_user_name"],
