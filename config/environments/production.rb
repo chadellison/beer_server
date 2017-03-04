ce4ae23d8bffa85c535e0f8dea1d09a35b46e1d3
Chad Ellison <chad.ellison0123@gmail.com>
2017-03-03 17:16:51 -0700

    adjust config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,21 +89,15 @@ Rails.application.configure do
   #   password:       ENV["email_password"]
   # }
 
-  config.action_mailer.default_url_options = { :host => 'https://beer-server0123.herokuapp.com' }
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.perform_deliveries = true
-    config.action_mailer.raise_delivery_errors = false
-    config.action_mailer.default :charset => "utf-8"
-    config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 587,
-      domain: "https://beer-server0123.herokuapp.com",
-      authentication: "plain",
-      enable_starttls_auto: true,
-      user_name: ENV["email_user_name"],
-      password: ENV["email_password"]
-    }
-
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
   config.action_mailer.default_url_options = { host: ENV["email_host"] }
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
