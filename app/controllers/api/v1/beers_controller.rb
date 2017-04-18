df4cc53918a89a271751e3216a54d4c8b4216448
Chad Ellison <chad.ellison0123@gmail.com>
2017-04-18 12:04:05 -0600

    Refactor beers_controller move logic into before actions

--- app/controllers/api/v1/beers_controller.rb
+++ app/controllers/api/v1/beers_controller.rb
@@ -2,46 +2,45 @@ module Api
   module V1
     class BeersController < Api::V1::BaseController
       before_action :authenticate_with_token, only: [:create]
+      before_action :find_or_initialize_beer, only: [:create]
+      before_action :filter_beers, only: [:index]
       respond_to :json
 
       def index
-        beer = Beer.current_beers(
-          search_params[:current_beers], search_params[:token]
-        )
-                   .beer_type(search_params[:type])
-                   .beer_name(search_params[:text])
-                   .sort_by_criterion(search_params[:sort])
-                   .current_page(search_params[:page])
-                   .beer_order
-
-        respond_with beers: beer
+        respond_with beers: @filtered_beers
       end
 
       def create
-        beer = find_or_initialize_beer
-
-        if beer.save
-          Rating.create_with_relationships(@user, beer, beer_params[:rating])
-          respond_with beer, location: nil
+        if @beer.save
+          Rating.create_with_relationships(@user, @beer, beer_params[:rating])
+          respond_with @beer, location: nil
         else
-          errors = { errors: beer.errors }
+          errors = { errors: @beer.errors }
           respond_with errors, location: nil, status: 422
         end
       end
 
       private
 
+      def filter_beers
+        @filtered_beers = Beer.current_beers(search_params[:current_beers],
+                                             search_params[:token])
+                              .beer_type(search_params[:type])
+                              .beer_name(search_params[:text])
+                              .sort_by_criterion(search_params[:sort])
+                              .current_page(search_params[:page])
+                              .beer_order
+      end
+
       def find_or_initialize_beer
-        beer = Beer.find_or_initialize_by(
+        @beer = Beer.find_or_initialize_by(
           name: beer_params[:name].to_s.downcase,
           beer_type: beer_params[:beer_type].to_s.downcase,
           brand: beer_params[:brand].to_s.downcase
         )
 
-        if beer_params[:abv].present? && beer.abv.nil?
-          beer.abv = beer_params[:abv]
-        end
-        beer
+        return @beer unless beer_params[:abv].present? && @beer.abv.nil?
+        @beer.abv = beer_params[:abv]
       end
 
       def search_params
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,4 @@
+# beer model
 class Beer < ApplicationRecord
   validates_presence_of :name, :beer_type, :brand
   validates_uniqueness_of :name
@@ -5,80 +6,40 @@ class Beer < ApplicationRecord
   has_many :users, through: :user_beers
   has_many :ratings
 
-  scope :current_beers, -> (beer, token = nil) do
-    if beer == "my beers"
+  scope :current_beers, (lambda do |beer, token = nil|
+    if beer == 'my beers'
       User.find_by(password_digest: token).beers
     else
       where(approved: true)
     end
-  end
+  end)
 
-  scope :beer_type, -> (type) do
-    where(beer_type: type) unless ["all types", ""].include?(type)
-  end
+  scope :beer_type, (lambda do |type|
+    where(beer_type: type) unless ['all types', ''].include?(type)
+  end)
 
-  scope :beer_name, -> (text) do
-    where("name LIKE ?", "%#{text.downcase}%") if text.present?
-  end
+  scope :beer_name, (lambda do |text|
+    where('name LIKE ?', "%#{text.downcase}%") if text.present?
+  end)
 
-  scope :sort_by_criterion, -> (criterion) do
+  scope :sort_by_criterion, (lambda do |criterion|
     case criterion
-    when "name" then order(:name)
-    when "rating" then order(average_rating: :desc)
-    when "abv" then order(abv: :desc)
+    when 'name' then order(:name)
+    when 'rating' then order(average_rating: :desc)
+    when 'abv' then order(abv: :desc)
     end
+  end)
+
+  scope :current_page, (lambda do |page|
+    offset((page.to_i - 1) * 24) if page.present?
+  end)
+
+  scope :beer_order, (-> { order(updated_at: :desc).limit(24) })
+
+  def self.fetch_beer_types(params)
+    current_beers(params[:current_beers], params[:token])
+      .select(:beer_type)
+      .distinct.pluck(:beer_type)
+      .unshift('all types')
   end
-
-  scope :current_page, -> (page) { offset((page.to_i - 1) * 24) if page.present? }
-  scope :beer_order, -> { order(updated_at: :desc).limit(24) }
-
-  # class << self
-  #   def search(params)
-  #     beers = scope_beers(params)
-  #     beers = beers.where(beer_type: params["type"]) unless ["all types", ""].include?(params["type"])
-  #     beers = check_text(params["text"], beers) if params["text"].present?
-  #     beers = sort_by(beers, params["sort"]) if params["sort"].present?
-  #     beers = calculate_offset(beers, params) if params[:page].present?
-  #     beers = beers.order(updated_at: :desc).limit(24)
-  #     beers
-  #   end
-  #
-  #   def check_text(text, beers)
-  #     beers.where("name LIKE ?", "%#{text.downcase}%")
-  #   end
-  #
-  #   def scope_to_user(token)
-  #     User.find_by(password_digest: token).beers
-  #   end
-  #
-  #   def sort_by(beers, criterion)
-  #     if criterion == "name"
-  #       beers.order(:name)
-  #     elsif criterion == "rating"
-  #       beers.order(average_rating: :desc)
-  #     elsif criterion == "abv"
-  #       beers.order(abv: :desc)
-  #     end
-  #   end
-  #
-  #   def fetch_beer_types(params)
-  #     scope_beers(params)
-  #       .select(:beer_type)
-  #       .distinct.pluck(:beer_type)
-  #       .unshift("all types")
-  #   end
-  #
-  #   def calculate_offset(beers, params)
-  #     offset = (params[:page].to_i - 1) * 24
-  #     beers.offset(offset)
-  #   end
-  #
-  #   def scope_beers(params)
-  #     if params["current_beers"] == "my beers"
-  #       scope_to_user(params[:token])
-  #     else
-  #       Beer.where(approved: true)
-  #     end
-  #   end
-  # end
 end
